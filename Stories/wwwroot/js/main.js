21a99c7a0393206c4b5a464727b8251a7123aa08
otngotalone99 <56649477+otngotalone99@users.noreply.github.com>
2021-01-05 21:05:36 +0700

    meta tags

--- Stories/wwwroot/js/main.js
+++ Stories/wwwroot/js/main.js
@@ -5,7 +5,7 @@
     $(window).on('load', function() {
         $('.preloader').delay(450).fadeOut('slow');
 
-        // Block somee ads
+        // Block Somee ads https://stackoverflow.com/questions/18999611/remove-auto-generated-advertisement-script-appended-to-the-results-returned-by-a
         $("div[style='opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;']").remove();
         $("div[style='margin: 0px; padding: 0px; left: 0px; width: 100%; height: 65px; right: 0px; bottom: 0px; display: block; position: fixed; z-index: 2147483647; opacity: 0.9; background-color: rgb(32, 32, 32);']").remove();
         $("div[onmouseover='S_ssac();']").remove();
